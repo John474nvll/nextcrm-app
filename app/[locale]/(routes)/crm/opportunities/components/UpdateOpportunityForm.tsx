2b3649cf604d91ec0e9f7c8cfb2efc28c2df440c
john474nvallejo <john474nvallejo@gmail.com>
2026-01-13 22:18:45 +0000

    ok

--- app/[locale]/(routes)/crm/opportunities/components/UpdateOpportunityForm.tsx
+++ app/[locale]/(routes)/crm/opportunities/components/UpdateOpportunityForm.tsx
@@ -152,7 +152,7 @@ export function UpdateOpportunityForm({
                   <FormControl>
                     <Input
                       disabled={isLoading}
-                      placeholder="New NextCRM functionality"
+                      placeholder="New SoftGAN functionality"
                       {...field}
                     />
                   </FormControl>
@@ -210,7 +210,7 @@ export function UpdateOpportunityForm({
                   <FormControl>
                     <Textarea
                       disabled={isLoading}
-                      placeholder="New NextCRM functionality"
+                      placeholder="New SoftGAN functionality"
                       {...field}
                     />
                   </FormControl>
