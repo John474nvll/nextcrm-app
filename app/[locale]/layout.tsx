3d510826de412cbdae1782d7e1059408f144a07e
john474nvallejo <john474nvallejo@gmail.com>
2026-01-13 22:23:41 +0000

    ok

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -65,24 +65,24 @@ export default async function RootLayout({ children, params: { locale } }: Props
           name="viewport"
           content="width=device-width, height=device-height, initial-scale=1"
         />
-        <meta property="og:url" content="https://www.nextcrm.io" />
+        <meta property="og:url" content="https://www.softgan.io" />
         <meta property="og:type" content="website" />
-        <meta property="og:title" content="NextCRM" />
+        <meta property="og:title" content="SoftGAN" />
         <meta
           property="og:description"
-          content="NextCRM is an open source CRM build on top of NextJS. Technology stack: NextJS with Typescrtipt, MongoDB, TailwindCSS, React, Prisma, shadCN, resend.com, react.email and more."
+          content="SoftGAN is an open source CRM build on top of NextJS. Technology stack: NextJS with Typescrtipt, MongoDB, TailwindCSS, React, Prisma, shadCN, resend.com, react.email and more."
         />
-        <meta property="og:image" content="https://nextcrm.io/api/og" />
+        <meta property="og:image" content="https://softgan.io/api/og" />
 
         <meta name="twitter:card" content="summary_large_image" />
-        <meta property="twitter:domain" content="nextcrm.io" />
-        <meta property="twitter:url" content="https://www.nextcrm.io" />
-        <meta name="twitter:title" content="NextCRM" />
+        <meta property="twitter:domain" content="softgan.io" />
+        <meta property="twitter:url" content="https://www.softgan.io" />
+        <meta name="twitter:title" content="SoftGAN" />
         <meta
           name="twitter:description"
-          content="NextCRM is an open source CRM build on top of NextJS. Technology stack: NextJS with Typescrtipt, MongoDB, TailwindCSS, React, Prisma, shadCN, resend.com, react.email and more."
+          content="SoftGAN is an open source CRM build on top of NextJS. Technology stack: NextJS with Typescrtipt, MongoDB, TailwindCSS, React, Prisma, shadCN, resend.com, react.email and more."
         />
-        <meta name="twitter:image" content="https://nextcrm.io/api/og" />
+        <meta name="twitter:image" content="https://softgan.io/api/og" />
       </head>
       <body className={inter.className + " min-h-screen"}>
         <NextIntlClientProvider locale={locale} messages={messages}>
