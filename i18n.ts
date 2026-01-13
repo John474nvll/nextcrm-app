c69df7b5fb52b3fd92b55e75e8ebab18955a8802
john474nvallejo <john474nvallejo@gmail.com>
2026-01-13 23:13:08 +0000

    ok

--- i18n.ts
+++ i18n.ts
@@ -1,19 +1,15 @@
-import { getRequestConfig, requestLocale } from "next-intl/server";
-import { notFound } from "next/navigation";
+import {getRequestConfig} from 'next-intl/server';
+import {notFound} from 'next/navigation';
 
-const locales = ["en", "de", "cz", "uk", "es"];
+// Can be imported from a shared config
+const locales = ['en', 'de', 'cz', 'uk', 'es'];
 
-export default getRequestConfig(async () => {
-  const locale = await requestLocale();
-
+export default getRequestConfig(async ({locale}) => {
   // Validate that the incoming `locale` parameter is valid
-  if (!locales.includes(locale as any)) {
-    notFound();
-  }
+  if (!locales.includes(locale as any)) notFound();
 
   return {
-    locale,
     messages: (await import(`./locales/${locale}.json`)).default,
-    timeZone: "Europe/Prague",
+    timeZone: 'Europe/Prague'
   };
 });
